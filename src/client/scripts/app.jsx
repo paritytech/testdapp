affefd424259609dda2c7438f98ba8ecc448b154
Gav <gavin@parity.io>
2017-10-23 16:55:58 +0200

    Changes for test.

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {bonds} from 'oo7-parity';
+import {Rspan} from 'oo7-react';
 
 export class App extends React.Component {
 	render() {
 		return (<div>
 			Hello world!
+			<Rspan>{bonds.blockNumber}</Rspan>
 		</div>);
 	}
 }
