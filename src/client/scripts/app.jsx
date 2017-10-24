d4da4b2fae874f7813d2a0655e5f1b56220ff611
Gav <gavin@parity.io>
2017-10-24 15:50:15 +0200

    Extra stuff.

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {bonds} from 'oo7-parity';
 import {Rspan} from 'oo7-react';
+import {InlineAccount} from 'parity-reactive-ui';
 
 export class App extends React.Component {
 	render() {
 		return (<div>
-			Hello world!
-			<Rspan>{bonds.blockNumber}</Rspan>
+			Hello world! Block number is <Rspan>{bonds.blockNumber}</Rspan>.
 		</div>);
 	}
 }
+
+window.bonds = bonds;
